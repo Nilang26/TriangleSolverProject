df41f60061e523a8596ae22d154995954c56a3fd
nilang26 <nilang926@gmail.com>
2024-03-06 13:21:44 -0500

    second commit (Iscosceles test passed)

--- TestTriangle/TriangleTest.cs
+++ TestTriangle/TriangleTest.cs
@@ -28,7 +28,50 @@ namespace TestTriangle
                     //assert
                     Assert.That(actual, Is.EqualTo(expected));
                 }
+
             }
+        [Test]
+        public void AnalyzeTriangle_input5and5and3_OutputIsoscelesTriangleIsFormed()
+        {
+            //Arrange
+            int side1 = 5, side2 = 5, side3 = 3;
+            string expected = "An isosceles triangle is formed";
+
+            //act
+            string actual = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            //assert
+            Assert.That(actual, Is.EqualTo(expected));
         }
+
+        [Test]
+        public void AnalyzeTriangle_input10and10and12_OutputIsoscelesTriangleIsFormed()
+        {
+            //Arrange
+            int side1 = 10, side2 = 10, side3 = 12;
+            string expected = "An isosceles triangle is formed";
+
+            //act
+            string actual = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            //assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+        [Test]
+        public void AnalyzeTriangle_input4and4and3_OutputIsoscelesTriangleIsFormed()
+        {
+            //Arrange
+            int side1 = 4, side2 = 4, side3 = 3;
+            string expected = "An isosceles triangle is formed";
+
+            //act
+            string actual = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            //assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+    }
+
     }
 
